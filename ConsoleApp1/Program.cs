14d0bc90237ad8ae294ffc944d5f4d8582f4a9d7
Via Aprillya <viaaprillya28@gmail.com>
2022-09-20 16:42:35 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DBConnectionExercise
+
+{
+    class Program
+    {
+        SqlConnection sqlConnection;
+        string connectionString = "Data Source=TARDIS;Initial Catalog=DTSMCC;User ID=viaaprillya;Password=1234567";
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+
+            //program.GetDepartment();
+            //program.InsertDivisions("Division 4");
+            program.UpdateDivisionsName(3, "Division Three");
+            //program.deleteDivisions(4);
+            program.GetDivisions();
+
+
+
+        }
+
+        void GetDepartment()
+        {
+            string query = "SELECT * FROM Departments";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void GetDivisions()
+        {
+            string query = "SELECT * FROM Divisions";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void GetById(int id)
+        {
+            string query = "SELECT * FROM Department WHERE Id = @id";
+
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@id";
+            sqlParameter.Value = id;
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(sqlParameter);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void InsertDivisions(string divName)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                sqlParameter.Value = divName;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "INSERT INTO Divisions " +
+                        "(DivName) VALUES (@name)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+        // Update Nama Division berdasarkan ID
+        void UpdateDivisionsName(int id, string newName)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter parameter1 = new SqlParameter();
+                parameter1.ParameterName = "@name";
+                parameter1.Value = newName;
+
+                SqlParameter parameter2 = new SqlParameter();
+                parameter2.ParameterName = "@id";
+                parameter2.Value = id;
+
+                sqlCommand.Parameters.Add(parameter1);
+                sqlCommand.Parameters.Add(parameter2);
+
+                try
+                {
+                    sqlCommand.CommandText = "UPDATE Divisions SET " +
+                        "DivName=@name WHERE DivisionID=@id";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+        // Delete Divisions berdasarkan ID
+        void deleteDivisions(int id)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@id";
+                sqlParameter.Value = id;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "DELETE FROM Divisions " +
+                        "WHERE DivisionID=@id";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+    }
+}
